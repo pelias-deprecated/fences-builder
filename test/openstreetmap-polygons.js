571c626219e4ca6e1f64a0e46c93b197d6a83ade
Diana Shkolnikov <dianashk@gmail.com>
2015-02-12 16:00:40 -0500

    Improved performance by refactoring the filter code. Added optional error dump to a file.

--- test/openstreetmap-polygons.js
+++ test/openstreetmap-polygons.js
@@ -1,51 +1,76 @@
 var fs = require('fs');
 var should = require('should');
-var osm_boundaries = require('./../');
+var polygons = require('./../');
 
+var expectedOutputFilePath = './test/data/test_expected_output.geojson';
+var expectedErrorFilePath = './test/data/test_expected_errors.geojson';
 
-var outputFilePath = './test/data/test_output.geojson';
+var inputFilePath = 'https://s3.amazonaws.com/metro-extracts.mapzen.com/asti_italy.osm.pbf';
+var outputFileDir = './test/data';
+var outputFilePath = outputFileDir + '/asti_italy.osm.pbf.geojson';
+var errorFilePath = outputFileDir + '/asti_italy.osm.pbf.err.json';
 
-describe('openstreetmap-boundaries', function () {
+describe('openstreetmap-polygons', function () {
 
-  describe('integration', function () {
+  it('should produce expected results, no error file', function (done) {
+    this.timeout(0);
 
-    it('should process an input file and produce expected results', function (done) {
-      this.timeout(0);
+    var options = {
+      inputFile: inputFilePath,
+      inputType: 'pbf',
+      outputDir: outputFileDir
+    };
 
-      var options = {
-        inputFile: 'https://s3.amazonaws.com/metro-extracts.mapzen.com/asti_italy.osm.pbf',
-        inputType: 'pbf',
-        outputFile: outputFilePath,
-        filterTags: {
-          boundary: {administrative: true}
+    polygons.extractPolygons(options, function (err, results) {
+      should.not.exist(err, 'Callback returns with no error');
+
+      var expectedStats = {
+        boundary_stream: {
+          errorCount: 29,
+          areaCount: 3109
+        },
+        filter: {
+          matched: 11,
+          errors: 1
         }
       };
 
-      osm_boundaries(options, function (err, results) {
-        should.not.exist(err, 'Callback returns with no error');
+      results.filter.should.be.eql(expectedStats.filter);
+      results.boundary_stream.areaCount.should.be.eql(expectedStats.boundary_stream.areaCount);
+      results.boundary_stream.errorCount.should.be.eql(expectedStats.boundary_stream.errorCount);
 
-        var expectedStats = {
-          boundary_stream: {
-            errorCount: 29,
-            areaCount: 3109
-          },
-          tag_filter: {
-            matched: 11,
-            discarded: 3098
-          }
-        };
+      var expected = JSON.parse(fs.readFileSync(expectedOutputFilePath));
+      var actual = JSON.parse(fs.readFileSync(outputFilePath));
+      actual.should.be.eql(expected);
 
-        results.should.be.eql(expectedStats, 'Validate stream stats');
+      // delete temp file
+      fs.unlinkSync(outputFilePath);
 
-        var expected = JSON.parse(fs.readFileSync('./test/data/test_expected_output.geojson'));
-        var actual = JSON.parse(fs.readFileSync(outputFilePath));
-        actual.should.be.eql(expected, 'Validate GEOJSON output file against expected');
+      done();
+    });
+  });
 
-        // delete temp file
-        fs.unlinkSync(outputFilePath);
 
-        done();
-      });
+  it('should produce error file', function (done) {
+    this.timeout(0);
+
+    var options = {
+      inputFile: inputFilePath,
+      inputType: 'pbf',
+      outputDir: outputFileDir,
+      errorDump: true
+    };
+
+    polygons.extractPolygons(options, function () {
+      var expected = JSON.parse(fs.readFileSync(expectedErrorFilePath));
+      var actual = JSON.parse(fs.readFileSync(errorFilePath));
+      actual.should.be.eql(expected);
+
+      // delete temp file
+      fs.unlinkSync(outputFilePath);
+      fs.unlinkSync(errorFilePath);
+
+      done();
     });
   });
 });
